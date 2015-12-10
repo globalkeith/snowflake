ae95e5886dac25abf584ef3aa47362b9dc4b0f9a
Keith Salisbury <keith.salisbury@alliants.com>
2015-12-10 23:38:36 +0000

    wip

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1285,97 +1285,6 @@ for (var i = 0; i < len; ++i) {
 
 var _game = require('./game');
 
-window.nextLevelSnowflake = (function () {
-  'use strict';
-
-  function nextLevelSnowflake(array, fractal) {
-    var newArray = [];
-    var end = array.length - 1;
-    var generatedPoints = [];
-
-    for (var i = 0; i < end; i++) {
-      var thisPoint = array[i];
-      var nextPoint = array[i + 1];
-
-      newArray.push(thisPoint);
-
-      generatedPoints = fractal(thisPoint, nextPoint);
-
-      for (var j = 0; j < generatedPoints.length; j++) {
-        newArray.push(generatedPoints[j]);
-      }
-    }
-
-    var thisPoint = array[end];
-    var nextPoint = array[0];
-
-    newArray.push(thisPoint);
-    var generatedPoints = fractal(thisPoint, nextPoint);
-
-    for (var j = 0; j < generatedPoints.length; j++) {
-      newArray.push(generatedPoints[j]);
-    }
-
-    return newArray;
-  }
-
-  return nextLevelSnowflake;
-}).call(undefined);
-
-function redrawSnowflake(array) {
-  graphics.moveTo(array[0].x, array[0].y);
-  for (var i = 1; i < array.length; i++) {
-    var point = array[i];
-    graphics.lineTo(point.x, point.y);
-  }
-
-  graphics.endFill();
-}
-
-window.initializeFractalGenerator = function initializeFractalGenerator() {
-  var datumX = 100;
-  var datumY = 200;
-  var side = 150;
-  var array = [{ x: datumX, y: datumY }, { x: datumX + 1 * side, y: datumY - side * Math.sqrt(3) }, { x: datumX + 2 * side, y: datumY }];
-
-  var equilateralTriangleHeightToSideRatio = Math.sqrt(3) / 2;
-
-  window.onclick = function () {
-    array = nextLevelSnowflake(array, function (start, end) {
-      var startToEnd = {
-        x: end.x - start.x,
-        y: end.y - start.y
-      };
-      var length = Math.sqrt(startToEnd.x * startToEnd.x + startToEnd.y * startToEnd.y);
-
-      var dir = {
-        x: startToEnd.x / length,
-        y: startToEnd.y / length
-      };
-
-      var perp = {
-        x: dir.y * length * equilateralTriangleHeightToSideRatio / 3,
-        y: -dir.x * length * equilateralTriangleHeightToSideRatio / 3
-      };
-
-      return [{
-        x: start.x + startToEnd.x / 3,
-        y: start.y + startToEnd.y / 3
-      }, {
-        x: start.x + perp.x + startToEnd.x / 2,
-        y: start.y + perp.y + startToEnd.y / 2
-      }, {
-        x: start.x + 2 * startToEnd.x / 3,
-        y: start.y + 2 * startToEnd.y / 3
-      }];
-    });
-
-    redrawSnowflake(array);
-  };
-
-  redrawSnowflake(array);
-};
-
 // Lets go!
 new _game.Game(700, 450, 'phaser');
 });
@@ -1532,19 +1441,19 @@ module.exports = exports["default"];
 });
 
 ;require.register("web/static/js/states/menu", function(exports, require, module) {
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var MenuState = (function (_Phaser$State) {
   _inherits(MenuState, _Phaser$State);
@@ -1552,65 +1461,193 @@ var MenuState = (function (_Phaser$State) {
   function MenuState() {
     _classCallCheck(this, MenuState);
 
-    _get(Object.getPrototypeOf(MenuState.prototype), "constructor", this).apply(this, arguments);
+    _get(Object.getPrototypeOf(MenuState.prototype), 'constructor', this).apply(this, arguments);
   }
 
   _createClass(MenuState, [{
-    key: "preload",
+    key: 'preload',
     value: function preload() {
-      // this.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/Fire.js');
+      this.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/Fire.js');
     }
   }, {
-    key: "create",
+    key: 'labelClicked',
+    value: function labelClicked() {
+      this.array = this.nextLevelSnowflake(this.array, this.fractal);
+      this.drawSnowflake();
+    }
+  }, {
+    key: 'create',
     value: function create() {
       // const label = this.addText("Snow flakes")
       // label.anchor.setTo(0.5)
       // label.y = 150
 
-      // let background = this.game.add.sprite(0, 0)
-      // background.width = 800
-      // background.height = 600
+      var background = this.game.add.sprite(0, 0);
+      background.width = 800;
+      background.height = 600;
 
-      var graphics = this.game.add.graphics(100, 100);
+      var bmd = this.game.add.bitmapData(800, 600);
+      var color = 'white';
 
-      // set a fill and line style
-      graphics.beginFill(null);
-      graphics.lineStyle(2, 0xffd900, 1);
-      // graphics.lineTo(250, 220)
-      // graphics.lineTo(50, 220)
-      // graphics.lineTo(50, 50)
+      bmd.ctx.beginPath();
+      bmd.ctx.lineWidth = "4";
+      bmd.ctx.strokeStyle = color;
+      bmd.ctx.stroke();
+      var sprite = this.game.add.sprite(0, 0, bmd);
 
-      window.graphics = graphics;
+      bmd.clear();
+      bmd.ctx.beginPath();
+      bmd.ctx.moveTo(10, 10);
+      bmd.ctx.lineTo(100, 100);
+      bmd.ctx.lineWidth = 4;
+      bmd.ctx.stroke();
+      bmd.ctx.closePath();
+      bmd.render();
+      bmd.refreshBuffer();
 
-      window.initializeFractalGenerator();
+      //this.bitmapData.lineStyle(2, 0xffd900, 1)
+      //  Draw a circle to it
+      //  The first 2 paramters are the x/y coordinates of the center
+      //  The third is the radius (100px)
+      // bmd.circle(150, 150, 150, 'rgb(0,200,0)');
 
+      // var ctx=bmd.context;
+      // // ctx.setLineWidth(5.0);
+      // // ctx.setFillColor(255,0,0,0);
+      // ctx.fillStyle = '#999999';
+      // ctx.lineWidth = 2;
+      // ctx.beginPath();
+      // ctx.moveTo(0, 0);
+      // ctx.lineTo(100, 100);
+      // ctx.lineTo(250, 100);
+      // this.game.add.sprite(50, 50, bmd);
+
+      // var graphics = this.game.add.graphics(100, 100)
+      // this.game.input.onDown.add(this.labelClicked, this);
+      //
+      // // graphics.events.onInputDown.add(this.labelClicked, this)
+      // // // set a fill and line style
+      // graphics.lineStyle(2, 0xffd900, 1)
+      // // // graphics.lineTo(250, 220)
+      // // // graphics.lineTo(50, 220)
+      // // // graphics.lineTo(50, 50)
+      // //
+      // // window.graphics = graphics;
+      // //
+      // // window.initializeFractalGenerator();
+      //
+      // var datumX = 100;
+      // var datumY = 200;
+      // var side = 150;
+      // var pointsArray = [
+      //   { x: datumX, y: datumY },
+      //   { x: datumX + 1*side, y: datumY - side * Math.sqrt(3) },
+      //   { x: datumX + 2*side, y: datumY }
+      // ];
+      //
+      // this.array = pointsArray
+      // this.graphics = graphics
+      //
+      // this.drawSnowflake(pointsArray, graphics)
+      //
       // let filter = this.game.add.filter("Fire", 800, 600)
       // filter.alpha = 0.0
       //
       // background.filters = [filter]
-
-      // this.label = label
+      //
+      // // this.label = label
       // this.background = background
       // this.filter = filter
     }
   }, {
-    key: "addText",
+    key: 'nextLevelSnowflake',
+    value: function nextLevelSnowflake(array, fractal) {
+      var newArray = [];
+      var end = array.length - 1;
+      var generatedPoints = [];
+
+      for (var i = 0; i < end; i++) {
+        var thisPoint = array[i];
+        var nextPoint = array[i + 1];
+
+        newArray.push(thisPoint);
+
+        generatedPoints = fractal(thisPoint, nextPoint);
+
+        for (var j = 0; j < generatedPoints.length; j++) {
+          newArray.push(generatedPoints[j]);
+        }
+      }
+
+      var thisPoint = array[end];
+      var nextPoint = array[0];
+
+      newArray.push(thisPoint);
+      var generatedPoints = fractal(thisPoint, nextPoint);
+
+      for (var j = 0; j < generatedPoints.length; j++) {
+        newArray.push(generatedPoints[j]);
+      }
+
+      return newArray;
+    }
+  }, {
+    key: 'drawSnowflake',
+    value: function drawSnowflake() {
+      var graphics = this.graphics;
+      var array = this.array;
+      graphics.beginFill(null);
+
+      graphics.moveTo(array[0].x, array[0].y);
+      for (var i = 1; i < array.length; i++) {
+        var point = array[i];
+        graphics.lineTo(point.x, point.y);
+      }
+      graphics.lineTo(array[0].x, array[0].y);
+      graphics.endFill();
+    }
+  }, {
+    key: 'fractal',
+    value: function fractal(start, end) {
+      var d = { x: end.x - start.x, y: end.y - start.y };
+      var length = Math.sqrt(d.x * d.x + d.y * d.y);
+
+      var dir = {
+        x: d.x / length,
+        y: d.y / length
+      };
+
+      var equilateralTriangleHeightToSideRatio = Math.sqrt(3) / 2;
+
+      var perp = {
+        x: dir.y * length * equilateralTriangleHeightToSideRatio / 3,
+        y: -dir.x * length * equilateralTriangleHeightToSideRatio / 3
+      };
+
+      return [{
+        x: start.x + d.x / 3,
+        y: start.y + d.y / 3
+      }, {
+        x: start.x + perp.x + d.x / 2,
+        y: start.y + perp.y + d.y / 2
+      }, {
+        x: start.x + 2 * d.x / 3,
+        y: start.y + 2 * d.y / 3
+      }];
+    }
+  }, {
+    key: 'addText',
     value: function addText(message) {
       var style = arguments.length <= 1 || arguments[1] === undefined ? { font: "45px Arial Black", fill: "#ffffff" } : arguments[1];
 
       return this.add.text(this.world.centerX, this.world.centerY, message, style);
     }
-
-    // v2. ninja, cowboy, bear
   }, {
-    key: "update",
+    key: 'update',
     value: function update() {
       // this.filter.update()
       // this.menu.rotation += 0.02
     }
-  }, {
-    key: "onShout",
-    value: function onShout() {}
   }]);
 
   return MenuState;
--- web/static/js/states/menu.js
+++ web/static/js/states/menu.js
@@ -1,6 +1,11 @@
 export class MenuState extends Phaser.State {
   preload() {
-    // this.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/Fire.js');
+    this.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/Fire.js');
+  }
+
+  labelClicked() {
+    this.array = this.nextLevelSnowflake(this.array, this.fractal)
+    this.drawSnowflake()
   }
 
   create() {
@@ -8,43 +13,165 @@ export class MenuState extends Phaser.State {
     // label.anchor.setTo(0.5)
     // label.y = 150
 
-    // let background = this.game.add.sprite(0, 0)
-  	// background.width = 800
-  	// background.height = 600
+    let background = this.game.add.sprite(0, 0)
+  	background.width = 800
+  	background.height = 600
 
-    var graphics = this.game.add.graphics(100, 100)
+    let bmd = this.game.add.bitmapData(800, 600)
+    let color = 'white';
 
-    // set a fill and line style
-    graphics.beginFill(null)
-    graphics.lineStyle(2, 0xffd900, 1)
-    // graphics.lineTo(250, 220)
-    // graphics.lineTo(50, 220)
-    // graphics.lineTo(50, 50)
+    bmd.ctx.beginPath();
+    bmd.ctx.lineWidth = "4";
+    bmd.ctx.strokeStyle = color;
+    bmd.ctx.stroke();
+    let sprite = this.game.add.sprite(0, 0, bmd);
 
-    window.graphics = graphics;
+    bmd.clear();
+    bmd.ctx.beginPath();
+    bmd.ctx.moveTo(10, 10);
+    bmd.ctx.lineTo(100, 100);
+    bmd.ctx.lineWidth = 4;
+    bmd.ctx.stroke();
+    bmd.ctx.closePath();
+    bmd.render();
+    bmd.refreshBuffer();
 
-    window.initializeFractalGenerator();
+    //this.bitmapData.lineStyle(2, 0xffd900, 1)
+    //  Draw a circle to it
+    //  The first 2 paramters are the x/y coordinates of the center
+    //  The third is the radius (100px)
+    // bmd.circle(150, 150, 150, 'rgb(0,200,0)');
 
+    // var ctx=bmd.context;
+    // // ctx.setLineWidth(5.0);
+    // // ctx.setFillColor(255,0,0,0);
+    // ctx.fillStyle = '#999999';
+    // ctx.lineWidth = 2;
+    // ctx.beginPath();
+    // ctx.moveTo(0, 0);
+    // ctx.lineTo(100, 100);
+    // ctx.lineTo(250, 100);
+    // this.game.add.sprite(50, 50, bmd);
+
+    // var graphics = this.game.add.graphics(100, 100)
+    // this.game.input.onDown.add(this.labelClicked, this);
+    //
+    // // graphics.events.onInputDown.add(this.labelClicked, this)
+    // // // set a fill and line style
+    // graphics.lineStyle(2, 0xffd900, 1)
+    // // // graphics.lineTo(250, 220)
+    // // // graphics.lineTo(50, 220)
+    // // // graphics.lineTo(50, 50)
+    // //
+    // // window.graphics = graphics;
+    // //
+    // // window.initializeFractalGenerator();
+    //
+    // var datumX = 100;
+    // var datumY = 200;
+    // var side = 150;
+    // var pointsArray = [
+    //   { x: datumX, y: datumY },
+    //   { x: datumX + 1*side, y: datumY - side * Math.sqrt(3) },
+    //   { x: datumX + 2*side, y: datumY }
+    // ];
+    //
+    // this.array = pointsArray
+    // this.graphics = graphics
+    //
+    // this.drawSnowflake(pointsArray, graphics)
+    //
   	// let filter = this.game.add.filter("Fire", 800, 600)
   	// filter.alpha = 0.0
     //
   	// background.filters = [filter]
-
-    // this.label = label
+    //
+    // // this.label = label
     // this.background = background
     // this.filter = filter
+  }
+
+  nextLevelSnowflake(array, fractal) {
+    var newArray = [];
+    var end = array.length - 1;
+    var generatedPoints = [];
+
+    for (var i = 0; i < end; i++) {
+      var thisPoint = array[i];
+      var nextPoint = array[i + 1];
+
+      newArray.push(thisPoint);
+
+      generatedPoints = fractal(thisPoint, nextPoint);
+
+      for (var j = 0; j < generatedPoints.length; j++) {
+        newArray.push(generatedPoints[j]);
+      }
+    }
+
+    var thisPoint = array[end];
+    var nextPoint = array[0];
+
+    newArray.push(thisPoint);
+    var generatedPoints = fractal(thisPoint, nextPoint);
+
+    for (var j = 0; j < generatedPoints.length; j++) {
+      newArray.push(generatedPoints[j]);
+    }
+
+    return newArray;
+  }
+
+  drawSnowflake() {
+    var graphics = this.graphics
+    var array = this.array
+    graphics.beginFill(null)
+
+    graphics.moveTo(array[0].x, array[0].y);
+    for (var i = 1; i < array.length; i++) {
+      var point = array[i];
+      graphics.lineTo(point.x, point.y);
+    }
+    graphics.lineTo(array[0].x, array[0].y)
+    graphics.endFill()
+  }
+
+  fractal(start, end) {
+    var d = { x: end.x - start.x, y: end.y - start.y }
+    var length = Math.sqrt(d.x*d.x + d.y*d.y)
+
+    var dir = {
+      x: d.x / length,
+      y: d.y / length,
+    };
+
+    var equilateralTriangleHeightToSideRatio = Math.sqrt(3) / 2;
+
+    var perp = {
+      x: dir.y * length * equilateralTriangleHeightToSideRatio / 3,
+      y: -dir.x * length * equilateralTriangleHeightToSideRatio / 3,
+    };
+
+    return [{
+      x: start.x + d.x / 3,
+      y: start.y + d.y / 3,
+    },
+    {
+      x: start.x + perp.x + d.x / 2,
+      y: start.y + perp.y + d.y / 2,
+    },
+    {
+      x: start.x + 2*d.x/3,
+      y: start.y + 2*d.y/3,
+    }]
   }
 
   addText(message, style = { font: "45px Arial Black", fill: "#ffffff" }) {
     return this.add.text(this.world.centerX, this.world.centerY, message, style)
   }
 
-  // v2. ninja, cowboy, bear
 	update() {
     // this.filter.update()
     // this.menu.rotation += 0.02
   }
-
-  onShout() {}
-
 }
